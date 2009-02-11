a0fe4f75eb8e5619dc83c7ea63c9ca353d9e4b68
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-11 16:46:17 +0000

    Generate code skeleton from UML using script

---
+++ trunk/src/Tile.java
@@ -0,0 +1,12 @@
+public class Tile {
+//+getX(): int
+//+getY(): int
++getTerrain(): Terrain
++getItem(): Item
+//+getEntity(): Entity
++getAreaEffect(): AreaEffect
++getDecal(): Decal
++delegateDraw(Graphics g)
++isPassable():bool
+}
+
