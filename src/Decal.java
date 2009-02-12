b1cbd63b16c801aef80e58c4a900db87c5f37caf
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-12 06:29:23 +0000

    Broke it, fixed it

--- src/Decal.java
+++ src/Decal.java
@@ -1,12 +1,16 @@
 /*
  * Decals augment the terrain and primarily serve as eye-candy. They do not
- * intrinsically affect game play—though one may be used to mark a tile to
+ * intrinsically affect game play -- though one may be used to mark a tile to
  * indicate an area-effect, &c.
  */
 
 public abstract class Decal implements Drawable {
 	
     protected final String name;	
+	
+	protected Decal(String name) {
+	    this.name = name;
+	}
 	
     public String toString() {
 	    return name;
