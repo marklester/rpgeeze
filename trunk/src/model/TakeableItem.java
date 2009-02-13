14d1640d7426681485e184e621d309b826c152ee
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-13 16:12:54 +0000

    TakeableItem: updated constructor.

--- trunk/src/model/TakeableItem.java
+++ trunk/src/model/TakeableItem.java
@@ -10,12 +10,8 @@ import view.Drawer;
 
 
 public abstract class TakeableItem extends Item {
-	public TakeableItem(Location location) {
-		super("Takeable Item",location);
-	}
-	
-	public void draw(Drawer d) {
-		d.drawTakeableItem(this);
+	public TakeableItem(String name, Location location) {
+		super(name,location);
 	}
 	
 	public void activate()
