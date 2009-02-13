4056e73725c7a739d9322a294263f5d8c19dc91f
jmoney38 <jmoney38@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-13 15:54:40 +0000

    Updated AEs and added Map stuff

--- src/model/AreaEffect.java
+++ src/model/AreaEffect.java
@@ -1,6 +1,26 @@
 package model;
 
-public interface AreaEffect {
+public abstract class AreaEffect {
+	
+	protected final String name;
+	protected float rate;
+	
+	//This will typically be used by LevelUp and InstantDeath - since their rates are irrelevant
+	public AreaEffect(String name) {
+		this.name = name;
+		rate = 0;
+	}
+	
+	public AreaEffect(float rate, String name){
+		this.name = name;
+		this.rate = rate;
+	}
+	
+	public void setRate(float rate) {
+		this.rate = rate;
+	}
+	
+	
 
 }
 
--- src/model/LevelUp.java
+++ src/model/LevelUp.java
@@ -1,6 +1,13 @@
 package model;
 
-public class LevelUp {
+public class LevelUp extends AreaEffect {
+
+	public LevelUp() {
+		super("Level Up");
+	}
+	public LevelUp(float rate) {
+		super(rate, "Level Up");
+	}
 
 }
 
