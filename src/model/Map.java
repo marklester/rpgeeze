66ceb86b9ab54ab5e56d43c393c0c559771e0c22
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-13 14:14:58 +0000

    Changed Map and Drawer to use an Iterator

--- src/model/Map.java
+++ src/model/Map.java
@@ -1,10 +1,14 @@
 package model;
 
 import java.io.*;
-import java.util.*;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import util.Iterator;
 
 public class Map {
-	public Tile[][] matrix;
+	private Tile[][] matrix;
 	
 	public Map(InputStream stream) {
 		Scanner s = new Scanner(stream);
@@ -25,6 +29,34 @@ public class Map {
 			list.add(arr);
 		}
 		matrix = list.toArray(new Tile[0][]);
+	}
+	
+	public Iterator<Tile> getTiles() {
+		return new Iterator<Tile>() {
+			private int x;
+			private int y;
+			
+			public void reset() {
+				x = 0;
+				y = 0;
+			}
+			
+			public boolean isDone() {
+				return y >= Map.this.matrix.length;
+			}
+			
+			public Tile current() {
+				return Map.this.matrix[x][y];
+			}
+			
+			public void advance() {
+				++x;
+				if(x == Map.this.matrix[0].length) {
+					++y;
+					x = 0;
+				}
+			}
+		};
 	}
 	
 	public Tile getTile(int x, int y) {
