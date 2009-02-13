463094f5b624691dfc143c4f513f24e5bbd43bff
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-13 13:52:05 +0000

    SkullAndCrossbones: added getInstance method.

--- src/model/SkullAndCrossbones.java
+++ src/model/SkullAndCrossbones.java
@@ -3,9 +3,15 @@ package model;
 import view.Drawer;
 
 public class SkullAndCrossbones extends Decal {
-	
+	private static SkullAndCrossbones instance = null;
 	public SkullAndCrossbones() {
 		super("Skull and Crossbones");
+	}
+	
+	public static SkullAndCrossbones getInstance() {
+		if(instance == null)
+			instance = new SkullAndCrossbones();
+		return instance;
 	}
 	
 	public void draw(Drawer d) {
