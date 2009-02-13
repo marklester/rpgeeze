e0624cb2adb9fb66786d4cd6a554ac566b5bbeb3
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-13 16:25:15 +0000

    Sword: added default constructor.

--- trunk/src/model/Sword.java
+++ trunk/src/model/Sword.java
@@ -5,17 +5,14 @@ import view.Drawer;
 public class Sword extends TakeableItem {
 	private static Sword instance = null;
 	
+	public Sword()
+	{
+	 super("Sword",new Location(0,0));
+	}
 	public Sword(Location location) {
 		super("Sword",location);
 	}
 	
-	public static Sword getInstance() {
-		if(instance == null){
-			Location location = new Location (0,0);
-			instance = new Sword(location);
-		}
-		return instance;
-	}
 	
 	public void draw(Drawer d) {
 		d.drawSword(this);
