62207ed1bdc0b39b02afbf581358c42874d3eeba
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-13 13:35:52 +0000

    RedCross: added getInstance method.

--- src/model/RedCross.java
+++ src/model/RedCross.java
@@ -3,11 +3,16 @@ package model;
 import view.Drawer;
 
 public class RedCross extends Decal {
-
+	private static RedCross instance = null;
 	public RedCross() {
 		super("Red Cross");
 	}
 
+	public static RedCross getInstance() {
+		if(instance == null)
+			instance = new RedCross();
+		return instance;
+	}
 	public void draw(Drawer d) {
 		d.drawRedCrossDecal(this);
 	}
