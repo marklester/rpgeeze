7ebdff7f126009a820e70cd96180d28de62038a3
jmoney38 <jmoney38@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-13 16:10:12 +0000

    crap

---
+++ trunk/src/model/HealDamage.java
@@ -0,0 +1,14 @@
+package model;
+
+public class HealDamage extends AreaEffect {
+
+	public HealDamage() {
+		super("Heal Damage");
+	}
+	
+	public HealDamage(float rate) {
+		super(rate, "Heal Damage");
+	}
+	
+}
+
