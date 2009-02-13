256e7d8d25c6c2b9d2b3ebe6626a4cd743204657
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-13 16:24:54 +0000

    Bolder: Added default constructor.

--- trunk/src/model/Bolder.java
+++ trunk/src/model/Bolder.java
@@ -4,18 +4,14 @@ import view.Drawer;
 
 
 public class Bolder extends Obstacle {
-	private static Bolder instance = null;
+	public Bolder()
+	{
+		super("Bolder",new Location (0,0));
+	}
 	public Bolder(Location location) {
 		super("Bolder",location);
 	}
 	
-	public static Bolder getInstance() {
-		if(instance == null){
-			Location location = new Location (0,0);
-			instance = new Bolder(location);
-		}
-		return instance;
-	}
 	
 	public void draw(Drawer d) {
 		d.drawBolder(this);
