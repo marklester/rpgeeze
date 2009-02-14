349019576506df25aeb85667557c8af37047ac37
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-14 14:37:16 +0000

    Sword: deleted default constructor b/c items need location.

--- trunk/src/model/Sword.java
+++ trunk/src/model/Sword.java
@@ -4,11 +4,6 @@ import view.Drawer;
 
 public class Sword extends TakeableItem {
 	
-	public Sword()
-	{
-	 super("Sword",new Location(0,0));
-	}
-	
 	public Sword(Location location) {
 		super("Sword",location);
 	}
