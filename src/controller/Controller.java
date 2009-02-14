b4c37b4e1efc79f356c8b85ebfd49233c400116b
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-14 18:09:31 +0000

    Controller: modified dropItem command

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -100,9 +100,8 @@ public class Controller extends JComponent {
 		this.getActionMap().put("UnequipRH", new ActionCommand(model, new UnequipCommand(model, Entity.ENT_RIGHT_H)));
 		this.getActionMap().put("UnequipHEAD", new ActionCommand(model, new UnequipCommand(model, Entity.ENT_HEAD)));
 		this.getActionMap().put("UnequipFEET", new ActionCommand(model, new UnequipCommand(model, Entity.ENT_FEET)));
-		this.getActionMap().put("DropITEM", new ActionCommand(model, new DropItemCommand(model)));
-		//For Stats;
-		*/
+		this.getActionMap().put("DropITEM", new ActionCommand(model, new ItemCommand(model,0)));
+		//For Stats;*/
 		this.getActionMap().put("Stats", new ActionCommand(model, new MenuCommand(model, controller,Controller.STAT_VIEW)));
 		this.getActionMap().put("Inventory", new ActionCommand(model, new MenuCommand(model, controller,Controller.INVENTORY_VIEW)));
 	}
