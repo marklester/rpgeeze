81d73bf0179389fa67210fe2e2426549aae6cd23
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-14 14:01:13 +0000

    Sword: got rid of instance variable.

--- trunk/src/model/Sword.java
+++ trunk/src/model/Sword.java
@@ -3,16 +3,15 @@ package model;
 import view.Drawer;
 
 public class Sword extends TakeableItem {
-	private static Sword instance = null;
 	
 	public Sword()
 	{
 	 super("Sword",new Location(0,0));
 	}
+	
 	public Sword(Location location) {
 		super("Sword",location);
 	}
-	
 	
 	public void draw(Drawer d) {
 		d.drawSword(this);
