255cad60fa5ab0da5ff601944105fb004b48bc3b
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-14 14:37:51 +0000

    Boulder: deleted default constructor b/c boulder needs a location.

--- src/model/Boulder.java
+++ src/model/Boulder.java
@@ -4,14 +4,10 @@ import view.Drawer;
 
 
 public class Boulder extends Obstacle {
-	public Boulder() {
-		super("Boulder",new Location (0,0));
-	}
 	
 	public Boulder(Location location) {
 		super("Boulder", location);
 	}
-	
 	
 	public void draw(Drawer d) {
 		d.drawBoulder(this);
