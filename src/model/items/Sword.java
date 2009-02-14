c756b71364817ab2c1b59da164450390f3adbdfb
mlester <mlester@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-14 16:03:45 +0000

    Moved to Here for cleaner structure I suppose

---
+++ src/model/items/Sword.java
@@ -0,0 +1,15 @@
+package model.items;
+
+import model.Location;
+import view.Drawer;
+
+public class Sword extends TakeableItem {
+	
+	public Sword(Location location) {
+		super("Sword",location);
+	}
+	
+	public void draw(Drawer d) {
+		d.drawSword(this);
+	}
+}
