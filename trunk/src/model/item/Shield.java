1b8bc6d651675d82cb95a362bd7555db7d3aeb6b
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-15 23:23:12 +0000

    Shield: added shield class

---
+++ trunk/src/model/item/Shield.java
@@ -0,0 +1,20 @@
+package model.item;
+
+/** 
+ * I haven't really looked into how this is being used, whoever added this
+ * should document it (i.e. I am too lazy to do it.)
+ */
+
+import model.Location;
+import view.Drawer;
+
+public class Shield extends TakeableItem {
+
+	public Shield(Location location) {
+		super("Shield", location);
+	}
+
+	public void draw(Drawer d) {
+		d.drawShield(this);
+	}
+}
