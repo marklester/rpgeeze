38711009fc1ea779cdc7550caf96f6c66f9a0ccf
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-15 22:33:27 +0000

    RunGame: added welcome screen code

--- src/RunGame.java
+++ src/RunGame.java
@@ -6,7 +6,12 @@ import view.Time;
 import view.View;
 import controller.Controller;
 import controller.OccupationSelector;
+import controller.WelcomeScreen;
 import util.ResourceLoader;
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Entry point to the entire game!
@@ -26,16 +31,29 @@ public class RunGame {
 	 * Starts a new game. See the description above for what happens.
 	 */
 	public static void main(String[] arg) {
-		Occupation occ = getOccupation();
-		Entity avatar = new Entity(occ);
-		Map map = new Map(ResourceLoader.getInstance().getStream("map.txt"));
-		Model model = new Model(map, avatar);
-
-		View view = new View(model);
-		//Controller controller = Controller.createController(model, view);
-
-		Time time = new Time(model, view);
-		time.start();
+		String w = new String("");
+		while(true)
+		{
+		 w = getWelcome();
+		if(w.equals("New"))
+		{
+		 break;
+		}
+		else if(w.equals("Load"))
+		{
+			String message = loadGame();
+			if(message.equals("Open"))
+			{
+			 break;
+			}
+		}
+		}
+		
+		if(w.equals("New"));
+		{
+			newGame();
+		}
+		
 	}
 
 	/**
@@ -54,4 +72,54 @@ public class RunGame {
 		}
 		return os.getOccupation();
 	}
+	
+	public static String getWelcome() {
+		WelcomeScreen welcome = new WelcomeScreen();
+		synchronized(welcome) {
+			try {
+				welcome.wait();
+			}
+			catch(InterruptedException e) {
+				
+			}
+		}
+		return welcome.getAction();
+	}
+	
+	public static void newGame()
+	{
+		Occupation occ = getOccupation();
+		 Entity avatar = new Entity(occ);
+		 Map map = new Map(ResourceLoader.getInstance().getStream("map.txt"));
+		 Model model = new Model(map, avatar);
+
+		 View view = new View(model);
+		 //Controller controller = Controller.createController(model, view);
+
+		 Time time = new Time(model, view);
+		 time.start();
+	}
+	
+	public static String loadGame()
+	{
+		String message = new String("");
+        JFileChooser chooser = new JFileChooser(); 
+		
+		//chooser.setFileFilter(new JFileChooser.FileFilter(new String[] { "rpg" }, "rpeeze_Files")); //creates file filter
+		    
+		    int status = chooser.showOpenDialog(null);
+		    
+		    if (status == JFileChooser.APPROVE_OPTION) {
+		      File selectedFile = chooser.getSelectedFile();
+		     message = "Open";
+		    } else if (status == JFileChooser.CANCEL_OPTION) {
+                message = "Cancel";
+                
+
+		    }
+		    return message;
+	}
+   
+	
+	
 }
