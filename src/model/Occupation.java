ab42f78329b581047e192cff6cb8d811e5cebe73
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-15 19:38:43 +0000

    Occupation: added stats as an attribute

--- src/model/Occupation.java
+++ src/model/Occupation.java
@@ -1,14 +1,23 @@
 package model;
 
-public abstract class Occupation {
+public abstract class Occupation implements Cloneable {
 	// OccupationType
 	// StatSet
 	protected final String name;
+	public Stats stats;
 
-	protected Occupation(String name) {
+	protected Occupation(String name, Stats stats) {
 		this.name = name;
+		this.stats = stats;
 	}
-
+	
+	public Occupation clone() throws CloneNotSupportedException{
+		Occupation o = (Occupation) super.clone();
+		o.stats = this.stats.clone();
+		return o;
+		
+	}
+	
 	public String toString() {
 		return this.name;
 	}
