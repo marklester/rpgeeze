5b14bbb75808688c53b8fdbaea2f136731a0493c
jmoney38 <jmoney38@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-15 22:54:38 +0000

    new  AE

--- src/model/TakeDamage.java
+++ src/model/TakeDamage.java
@@ -1,5 +1,7 @@
 package model;
 
+import view.Console;
+
 public class TakeDamage extends AreaEffect {
 
 	public TakeDamage() {
@@ -10,4 +12,14 @@ public class TakeDamage extends AreaEffect {
 		super(rate, "Take Damage");
 	}
 
+	public void applyEffect(Entity e) {
+		if (--counter == 0) {
+			e.getStats().decLife(rate);
+			counter = UPDATE_RATE; //reset
+			if (!f_msg_was_sent) {
+				Console.getInstance().writeLine("Yo dog, you're dieing");
+				f_msg_was_sent = true;
+			}
+		}
+	}
 }
