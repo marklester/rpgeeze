4ec94dfc7b7b6afe33cd18d7911fd2e759102ecc
jmoney38 <jmoney38@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-15 23:11:52 +0000

    moved

---
+++ trunk/src/model/ae/HealDamage.java
@@ -0,0 +1,26 @@
+package model;
+
+import view.Console;
+
+public class HealDamage extends AreaEffect {
+
+	public HealDamage() {
+		super("Heal Damage");
+	}
+
+	public HealDamage(int rate) {
+		super(rate, "Heal Damage");
+	}
+	
+	public void applyEffect(Entity e) {
+		if (--counter == 0) {
+			e.getStats().incLife(rate);
+			counter = UPDATE_RATE; //reset
+			if (!f_msg_was_sent) {
+				Console.getInstance().writeLine("Congrats. Enjoy some life");
+				f_msg_was_sent = true;
+			}
+		}
+	}
+
+}
