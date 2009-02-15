ddfa737a17eed40b6f19b244d6a78ed5cbb95a7d
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-15 22:18:08 +0000

    Items are now read as XML.

--- trunk/src/model/Location.java
+++ trunk/src/model/Location.java
@@ -1,9 +1,14 @@
 package model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Location {
 	private final int x;
 	private final int y;
 
+	private static Pattern pattern = Pattern.compile("<location><x>(.*?)</x><y>(.*)</y></location>");
+	
 	public Location() {
 		this.x = 0;
 		this.y = 0;
@@ -26,4 +31,25 @@ public class Location {
 		return "(" + this.x + "," + this.y + ")";
 	}
 
+	public String toXml() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<location>");
+		sb.append("<x>");
+		sb.append(x);
+		sb.append("</x>");
+		sb.append("<y>");
+		sb.append(y);
+		sb.append("</y>");
+		sb.append("</location>");
+		return sb.toString();
+	}
+	
+	public static Location fromXml(String xml) {
+		Matcher mat = pattern.matcher(xml);
+		if(!mat.matches())
+			throw new RuntimeException("Bad XML for Location");
+		int x = Integer.parseInt(mat.group(1));
+		int y = Integer.parseInt(mat.group(2));
+		return new Location(x, y);
+	}
 }
