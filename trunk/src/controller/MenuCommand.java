47b9eeaf273f81455b88748f2feff1f078933d0b
mlester <mlester@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-15 08:08:11 +0000

    Gets rid of controller in the constructor because this command doesn't need it.

--- trunk/src/controller/MenuCommand.java
+++ trunk/src/controller/MenuCommand.java
@@ -3,12 +3,10 @@ package controller;
 import model.*;
 //Used to control Menu Popups
 public class MenuCommand implements Command {
-	private final Controller controller;
 	private final Model m;
 	private final int viewport;
-	public MenuCommand(Model m, Controller controller,int viewport) {
+	public MenuCommand(Model m, int viewport) {
 		this.m = m;
-		this.controller = controller;
 		this.viewport=viewport;
 	}
 	
@@ -21,6 +19,12 @@ public class MenuCommand implements Command {
 					m.setStatsVisible(true);
 				}
 			break;
+			case Controller.INVENTORY_VIEW:
+				if(m.isInventoryUp()){
+					m.setInventoryVisible(false);
+				}else{
+					m.setInventoryVisible(true);
+				}
 			default:
 			break;
 		}
