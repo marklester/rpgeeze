1305eda7f061daab21d105e07e5a246f2741c13f
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-16 03:05:25 +0000

    Working on XML conversion

--- trunk/src/model/decal/Decal.java
+++ trunk/src/model/decal/Decal.java
@@ -13,7 +13,7 @@ import java.util.regex.Matcher;
 import view.Drawable;
 
 public abstract class Decal implements Drawable, Cloneable {
-	private static Pattern pattern = Pattern.compile("<decal>(.*)</decal>");
+	private static Pattern pattern = Pattern.compile("<decal><name>(.*)</name></decal>");
 	private static Hashtable<String, Decal> prototypes = new Hashtable<String, Decal>();
 	
 	static {
