9b24819a76d9348c0aa5e55f7fe672814dcca160
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-16 09:48:38 +0000

    added helmet class

---
+++ src/model/item/Helmet.java
@@ -0,0 +1,26 @@
+package model.item;
+
+/** 
+ * I haven't really looked into how this is being used, whoever added this
+ * should document it (i.e. I am too lazy to do it.)
+ */
+
+import model.Entity;
+import model.Location;
+import view.Drawer;
+
+public class Helmet extends TakeableItem {
+
+	public Helmet() { //Location location) {
+		super("Helmet");//, location);
+	}
+
+	public void draw(Drawer d) {
+		d.drawHelmet(this);
+	}
+	
+	@Override
+	public void activate(Entity e) {
+		e.equipHead(this);
+	}
+}
