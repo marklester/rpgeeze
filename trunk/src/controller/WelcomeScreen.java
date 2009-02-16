9e10fec0c7357f854e7d1818e859f70ae78e5512
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-16 18:21:16 +0000

    Executing project as jar now properly displays splash images

--- trunk/src/controller/WelcomeScreen.java
+++ trunk/src/controller/WelcomeScreen.java
@@ -1,6 +1,5 @@
 package controller;
 
-import javax.swing.Icon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,13 +10,7 @@ import javax.swing.WindowConstants;
 import javax.swing.ImageIcon;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
-
-import model.Occupation;
-import model.Smasher;
-import model.Summoner;
-import model.Sneak;
 import java.awt.Color;
-
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Dimension;
@@ -27,6 +20,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
+import model.Occupation;
+import model.Smasher;
+import model.Summoner;
+import model.Sneak;
+import util.ResourceLoader;
 
 public class WelcomeScreen extends JFrame {
 	private String action = new String("");
@@ -47,63 +45,55 @@ public class WelcomeScreen extends JFrame {
 		initFrame();
 	}
 	
-	public void initFrame()
-	{
+	public void initFrame() {
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		Dimension screen = tk.getScreenSize();
-        
-        
-        JButton newGame = new JButton(new ImageIcon("res/img/buttons/NewGame.png"));
+				
+        JButton newGame = new JButton(new ImageIcon(ResourceLoader.getInstance().getImage("img/buttons/NewGame.png")));
 		newGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
 				synchronized(WelcomeScreen.this) {
 					if(getAction() != null) {
 						setAction("New");
 						WelcomeScreen.this.notifyAll();
-						}
 					}
 				}
 			}
-		);
+		});
 		newGame.setOpaque(false);
 		newGame.setContentAreaFilled(false);
 		newGame.setBorderPainted(false);
 		newGame.setBorder(buttonBorder);
 		
-		JButton loadGame = new JButton(new ImageIcon("res/img/buttons/LoadGame.png"));
-		loadGame.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		JButton loadGame = new JButton(new ImageIcon(ResourceLoader.getInstance().getImage("img/buttons/LoadGame.png")));
+		loadGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				synchronized(WelcomeScreen.this) {
-				
 					JFileChooser chooser = new JFileChooser(); 
 					
-					    int status = chooser.showOpenDialog(null);
+					int status = chooser.showOpenDialog(null);
 					    
-					    if (status == JFileChooser.APPROVE_OPTION) {
-					      File selectedFile = chooser.getSelectedFile();
-					      if(getAction() != null) {
-								setAction("Open");
-								WelcomeScreen.this.setVisible(false);
-								WelcomeScreen.this.notifyAll();
-								WelcomeScreen.this.dispose();
-								
-					      }
-					    } 
-					    else if (status == JFileChooser.CANCEL_OPTION) {
-			                //message = "Cancel";
-					    	
+					if(status == JFileChooser.APPROVE_OPTION) {
+						File selectedFile = chooser.getSelectedFile();
+					    if(getAction() != null) {
+					    	setAction("Open");
+							WelcomeScreen.this.setVisible(false);
+							WelcomeScreen.this.notifyAll();
+							WelcomeScreen.this.dispose();
 					    }
-					
+					} 
+					else if(status == JFileChooser.CANCEL_OPTION) {
+						//message = "Cancel";					    	
+					}
 				}
 			}
-		  }
-		);
+		});
 		loadGame.setOpaque(false);
 		loadGame.setContentAreaFilled(false);
 		loadGame.setBorderPainted(false);
 		loadGame.setBorder(buttonBorder);
 		
-		JButton quitGame = new JButton(new ImageIcon("res/img/buttons/QuitGame.png"));
+		JButton quitGame = new JButton(new ImageIcon(ResourceLoader.getInstance().getImage("img/buttons/QuitGame.png")));
 		quitGame.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				synchronized(WelcomeScreen.this) {
@@ -131,7 +121,7 @@ public class WelcomeScreen extends JFrame {
 		panel.setBackground(mainScreenColor);
 		panel.add(p1, BorderLayout.SOUTH);
 		
-		JLabel l = new JLabel(" ",new ImageIcon("res/img/IntroOccupationTypes.png"),SwingConstants.CENTER);
+		JLabel l = new JLabel(" ", new ImageIcon(ResourceLoader.getInstance().getImage("img/IntroOccupationTypes.png")), SwingConstants.CENTER);
 		panel.add(l,BorderLayout.CENTER);
 		panel.setOpaque(true);
 		
@@ -147,82 +137,71 @@ public class WelcomeScreen extends JFrame {
 		
 	}
 	
-	public void initOcc()
-	{
+	public void initOcc() {
 		JPanel p2 = new JPanel(new FlowLayout());
-		JButton smasher = new JButton(new ImageIcon("res/img/buttons/Smasher.png"));
+		JButton smasher = new JButton(new ImageIcon(ResourceLoader.getInstance().getImage("img/buttons/Smasher.png")));
 		smasher.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				synchronized(WelcomeScreen.this) {
 					setOccupation(new Smasher());
 					WelcomeScreen.this.notifyAll();
-					}
 				}
 			}
-		
-		);
+		});
 		smasher.setBackground(mainScreenColor);
 		smasher.setBorderPainted(false);
 		smasher.setBorder(buttonBorder);
-		
-		JButton summoner = new JButton(new ImageIcon("res/img/buttons/Summoner.png"));
+
+		JButton summoner = new JButton(new ImageIcon(ResourceLoader.getInstance().getImage("img/buttons/Summoner.png")));
 		summoner.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				synchronized(WelcomeScreen.this) {
 					setOccupation(new Summoner());
 					WelcomeScreen.this.notifyAll();
-					}
 				}
 			}
-		);
+		});
 		summoner.setBackground(mainScreenColor);
 		summoner.setBorderPainted(false);
 		summoner.setBorder(buttonBorder);
 		
-		JButton sneak = new JButton(new ImageIcon("res/img/buttons/Sneak.png"));
+		JButton sneak = new JButton(new ImageIcon(ResourceLoader.getInstance().getImage("img/buttons/Sneak.png")));
 		sneak.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				synchronized(WelcomeScreen.this) {
 					setOccupation(new Sneak());
 					WelcomeScreen.this.notifyAll();
-					}
 				}
 			}
-		);
+		});
 
 		sneak.setBackground(mainScreenColor);
 		sneak.setBorderPainted(false);
 		sneak.setBorder(buttonBorder);
 		
-		 p2.add(smasher);
-		 p2.add(summoner);
-		 p2.add(sneak);
-		 p2.setBackground(new Color(254,72,72));
+		p2.add(smasher);
+		p2.add(summoner);
+		p2.add(sneak);
+		p2.setBackground(new Color(254, 72, 72));
 		 
-	 panel.remove(p1);
-	 panel.add(p2,BorderLayout.SOUTH);
-	 panel.revalidate();
-	 
+		panel.remove(p1);
+		panel.add(p2, BorderLayout.SOUTH);
+		panel.revalidate();
 	}
 	
-	public void setAction(String action)
-	{
+	public void setAction(String action) {
 		this.action = action;
 	}
 	
-	public String getAction()
-	{
+	public String getAction() {
 		return this.action;
 	}
 	
-	public void setOccupation(Occupation occ)
-	{
+	public void setOccupation(Occupation occ) {
 		this.occ = occ;
 	}
 	
-	public Occupation getOccupation()
-	{
+	public Occupation getOccupation() {
 		return occ;
 	}
-
 }
