6fc66b378e808b02feffbafeedefcd64f2042293
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-16 03:47:20 +0000

    added arrows class

---
+++ src/model/item/Arrows.java
@@ -0,0 +1,20 @@
+package model.item;
+
+/** 
+ * I haven't really looked into how this is being used, whoever added this
+ * should document it (i.e. I am too lazy to do it.)
+ */
+
+import model.Location;
+import view.Drawer;
+
+public class Arrows extends TakeableItem {
+
+	public Arrows(Location location) {
+		super("Arrows", location);
+	}
+
+	public void draw(Drawer d) {
+		d.drawArrows(this);
+	}
+}
