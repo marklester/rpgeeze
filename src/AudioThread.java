4bf69c5f909d523ed0fa35fb7d58123355fc37a7
jmoney38 <jmoney38@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-16 06:43:37 +0000

    Audio bologna - don't worry about it for now... Also, made stats adjustable

--- src/AudioThread.java
+++ src/AudioThread.java
@@ -21,82 +21,61 @@ public class AudioThread extends Thread {
 			if(!muted)
 			{
 				try {
-					SourceDataLine line = null;
-					InputStream in = new FileInputStream("res/audio/zelda1.mp3");
-					AudioInputStream ain = AudioSystem.getAudioInputStream(in);
-					AudioFormat format = ain.getFormat( );
-		            DataLine.Info info=new DataLine.Info(SourceDataLine.class,format);
-		            if (!AudioSystem.isLineSupported(info)) {
-		                // This is the PCM format we want to transcode to.
-		                // The parameters here are audio format details that you
-		                // shouldn't need to understand for casual use.
-		                AudioFormat pcm =
-		                    new AudioFormat(format.getSampleRate( ), 16,
-		                                    format.getChannels( ), true, false);
-
-		                // Get a wrapper stream around the input stream that does the
-		                // transcoding for us.
-		                ain = AudioSystem.getAudioInputStream(pcm, ain);
-
-		                // Update the format and info variables for the transcoded data
-		                format = ain.getFormat( ); 
-		                info = new DataLine.Info(SourceDataLine.class, format);
-		            }
-		            line = (SourceDataLine) AudioSystem.getLine(info);
-		            line.open(format);  
-		            
-		            // Allocate a buffer for reading from the input stream and writing
-		            // to the line.  Make it large enough to hold 4k audio frames.
-		            // Note that the SourceDataLine also has its own internal buffer.
-		            int framesize = format.getFrameSize( );
-		            byte[  ] buffer = new byte[4 * 1024 * framesize]; // the buffer
-		            int numbytes = 0;                               // how many bytes
-
-		            // We haven't started the line yet.
-		            boolean started = false;
-
-		            for(;;) {  // We'll exit the loop when we reach the end of stream
-		                // First, read some bytes from the input stream.
-		                int bytesread=ain.read(buffer,numbytes,buffer.length-numbytes);
-		                // If there were no more bytes to read, we're done.
-		                if (bytesread == -1) break;
-		                numbytes += bytesread;
-		                
-		                // Now that we've got some audio data to write to the line,
-		                // start the line, so it will play that data as we write it.
-		                if (!started) {
-		                    line.start( );
-		                    started = true;
-		                }
-		                
-		                // We must write bytes to the line in an integer multiple of
-		                // the framesize.  So figure out how many bytes we'll write.
-		                int bytestowrite = (numbytes/framesize)*framesize;
-		                
-		                // Now write the bytes. The line will buffer them and play
-		                // them. This call will block until all bytes are written.
-		                line.write(buffer, 0, bytestowrite);
-		                
-		                // If we didn't have an integer multiple of the frame size, 
-		                // then copy the remaining bytes to the start of the buffer.
-		                int remaining = numbytes - bytestowrite;
-		                if (remaining > 0)
-		                    System.arraycopy(buffer,bytestowrite,buffer,0,remaining);
-		                numbytes = remaining;
-		            }
-
-		            // Now block until all buffered sound finishes playing.
-		            line.drain( );
-
-		            
-		            
-					AudioStream as = new AudioStream(in);
-					AudioData data = as.getData();
-				}
-				catch (Exception e) {System.out.println(e);}
+					File file = new File("res/audio/zelda1.mp3");
+				    AudioInputStream in= AudioSystem.getAudioInputStream(file);
+				    AudioInputStream din = null;
+				    AudioFormat baseFormat = in.getFormat();
+				    AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+				                                                                                  baseFormat.getSampleRate(),
+				                                                                                  16,
+				                                                                                  baseFormat.getChannels(),
+				                                                                                  baseFormat.getChannels() * 2,
+				                                                                                  baseFormat.getSampleRate(),
+				                                                                                  false);
+				    din = AudioSystem.getAudioInputStream(decodedFormat, in);
+				    // Play now.
+				    rawplay(decodedFormat, din);
+				    in.close();
+				} catch (Exception e)
+				    {
+				        System.out.println(e);
+				    }
 			}
 		}
 	}
+	
+	private void rawplay(AudioFormat targetFormat, AudioInputStream din) throws IOException,                                                                                                LineUnavailableException
+	{
+	  byte[] data = new byte[4096];
+	  SourceDataLine line = getLine(targetFormat);
+	  if (line != null)
+	  {
+	    // Start
+	    line.start();
+	    int nBytesRead = 0, nBytesWritten = 0;
+	    while (nBytesRead != -1)
+	    {
+	        nBytesRead = din.read(data, 0, data.length);
+	        if (nBytesRead != -1) nBytesWritten = line.write(data, 0, nBytesRead);
+	    }
+	    // Stop
+	    line.drain();
+	    line.stop();
+	    line.close();
+	    din.close();
+	  }
+	}
+		
+	private SourceDataLine getLine(AudioFormat audioFormat) throws LineUnavailableException
+	{
+	  SourceDataLine res = null;
+	  DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+	  res = (SourceDataLine) AudioSystem.getLine(info);
+	  res.open(audioFormat);
+	  return res;
+	}
+
+		
 	
 	public void setMute(boolean b)
 	{
