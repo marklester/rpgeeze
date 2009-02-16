79cc5f9c02adae912b3bb7f640154d58f95d0c6a
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-02-16 03:26:01 +0000

    RedArmor: add red armor class

---
+++ src/model/item/RedArmor.java
@@ -0,0 +1,20 @@
+package model.item;
+
+/** 
+ * I haven't really looked into how this is being used, whoever added this
+ * should document it (i.e. I am too lazy to do it.)
+ */
+
+import model.Location;
+import view.Drawer;
+
+public class RedArmor extends TakeableItem {
+
+	public RedArmor(Location location) {
+		super("Red Armor", location);
+	}
+
+	public void draw(Drawer d) {
+		d.drawRedArmor(this);
+	}
+}
