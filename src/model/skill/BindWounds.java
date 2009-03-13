afd35a541f10b31a72dfc34cd429df5c1aea9bb5
jmoney38 <jmoney38@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-13 02:24:48 +0000

    Basic stuff for Skills. Disregard for now... I just need to transfer over to my other computer to keep working...

---
+++ src/model/skill/BindWounds.java
@@ -0,0 +1,15 @@
+package model.skill;
+
+public class BindWounds extends Skill implements UsableSkill {
+
+	
+	public BindWounds() {
+		super("Bind Wounds", "Heals damage - based on your skill level");
+	}
+	
+	public void use() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/model/skill/Skill.java
@@ -0,0 +1,20 @@
+package model.skill;
+
+public abstract class Skill {
+	
+	int points;
+	String name;
+	
+	public Skill(String name) {
+		this(name, "", 0);
+	}
+	
+	public Skill(String name, String descr) {
+		this(name, descr, 0);
+	}
+	
+	public Skill(String name, String descr, int initPoints) {
+		this.name = name;
+		this.points = initPoints;
+	}
+}
