478b3a184446a2a582bae2dd90a51e8225afcf6f
jmoney38 <jmoney38@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-17 02:45:02 +0000

    BindWounds now works. Added a sound effect too

--- src/model/skill/BindWounds.java
+++ src/model/skill/BindWounds.java
@@ -1,5 +1,14 @@
 package model.skill;
 
+import java.awt.Color;
+
+import view.Console;
+import model.Entity;
+import model.Stats;
+import model.PrimaryStats;
+import util.ResourceLoader;
+
+
 public class BindWounds extends Skill implements UsableSkill {
 
 	
@@ -10,6 +19,27 @@ public class BindWounds extends Skill implements UsableSkill {
 	public void use() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void use(Entity e) {
+			if (e.getStats().getMana() >= 10) {
+				ResourceLoader.getInstance().playAudioClip(this.name);
+				Console.getInstance().writeLine("Life is invigorating", Color.WHITE);
+				int life = e.getStats().getLife();
+				double perc = 0;
+				
+				if (points > 20)
+					perc = points * .01;
+				else
+					perc = (points * .01);
+				life = (int) (perc * life);
+				
+				e.getStats().incLife(life);
+				e.getStats().decMana(10);
+			}
+			else
+				Console.getInstance().writeLine("You do not have enough mana...", Color.RED);
+		
 	}
 
 }
