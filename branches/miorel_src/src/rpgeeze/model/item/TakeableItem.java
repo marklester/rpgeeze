ea58b6dd1e11843b510507abef9e2f80ab53341e
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-20 05:19:21 +0000

    Removed rpgeeze.model.phys package because it's overkill at this point. After we get everything done, it's coming back and we can go crazy.

--- branches/miorel_src/src/rpgeeze/model/item/TakeableItem.java
+++ branches/miorel_src/src/rpgeeze/model/item/TakeableItem.java
@@ -1,17 +1,12 @@
 package rpgeeze.model.item;
 
 import rpgeeze.model.Entity;
-import rpgeeze.model.phys.GameObject;
 
-public abstract class TakeableItem implements GameObject, Item {
+public abstract class TakeableItem implements Item {
 	public void activate(Entity entity) {
 		// add self to entity's inventory
 		
 		// don't worry about checking if there is space:
 		// by TDA entity will just ignore request to add to inventory if it's full 
-	}
-	
-	public double getMass() {
-		return 0;
 	}
 }
