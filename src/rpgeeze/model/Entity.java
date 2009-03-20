64d3a818f49209ceddad363d33da85c1ddc2bd0b
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-20 05:19:21 +0000

    Removed rpgeeze.model.phys package because it's overkill at this point. After we get everything done, it's coming back and we can go crazy.

--- src/rpgeeze/model/Entity.java
+++ src/rpgeeze/model/Entity.java
@@ -1,31 +1,14 @@
 package rpgeeze.model;
 
 import rpgeeze.math.Vector;
-import rpgeeze.model.phys.GameObject;
 import rpgeeze.util.Direction;
 
-public class Entity implements GameObject {
+public class Entity {
 	private Direction facing;
 	private Tile tile;
 	
 	public Vector getFacingDirection() {
 		return facing;
-	}
-	
-	public double getMass() {
-		return 1;
-	}
-
-	public Vector getAcceleration() {
-		return null;
-	}
-
-	public Vector getPosition() {
-		return null;
-	}
-
-	public Vector getVelocity() {
-		return null;
 	}
 
 	public Tile getTile() {
