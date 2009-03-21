d402416e2c63991234cfde4899e47ba29fcf550a
jmoney38 <jmoney38@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-21 22:28:51 +0000

    Minor tidying up

--- src/model/skill/TwoHandedWeapon.java
+++ src/model/skill/TwoHandedWeapon.java
@@ -1,13 +1,9 @@
 package model.skill;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import model.entity.Stats;
 
 public class TwoHandedWeapon extends Skill implements PermanentSkill {
 
-	private static final Pattern pattern = Pattern.compile("<twohandedweapon><points>(.*)</points></twohandedweapon>");
 	
 	public TwoHandedWeapon() {
 		this(0);
@@ -21,23 +17,5 @@ public class TwoHandedWeapon extends Skill implements PermanentSkill {
 		// TODO Auto-generated method stub
 
 	}
-	
-	/*
-	public static TwoHandedWeapon fromXml(String xml) {
-		Matcher mat = pattern.matcher(xml);
-		if(!mat.matches())
-			throw new RuntimeException("Bad XML for Two-handed Skill");
-		int points = Integer.parseInt(mat.group(1));
-		return new TwoHandedWeapon(points);
-	}
-	
-	public String toXml(String indent) {
-		StringBuilder sb = new StringBuilder();
-		sb.append(indent + "<tohandedweapon>\n");
-		sb.append(indent + "\t<points>" + this.getPoints() + "</points>\n");
-		sb.append(indent + "</twohandedweapon>");
-		return sb.toString();
-	}
-	*/
 
 }
