f82e12b94ac27a3b4d83b02c46cc63433634c87f
speedymorales5 <speedymorales5@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-21 13:58:10 +0000

    Fixed Jason's Broken 'RangedWeapon' Code

---
+++ src/model/skill/RangedWeapon.java
@@ -0,0 +1,21 @@
+package model.skill;
+
+import model.skill.visitor.Visitor;
+
+public class RangedWeapon extends Skill implements UsableSkill {
+
+		
+		public RangedWeapon() {
+			super("Ranged Weapon", "Does damage - based on your skill level");
+		}
+		
+		public void use() {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void accept(Visitor v) {
+			v.visit(this);
+			
+		}
+}
