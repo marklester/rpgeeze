485eff3fdafa8ee1492bc22dc65616e9daaa9bd6
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-22 13:48:48 +0000

    num pad keys

--- branches/miorel_src/src/rpgeeze/view/KeyBindingsView.java
+++ branches/miorel_src/src/rpgeeze/view/KeyBindingsView.java
@@ -134,6 +134,10 @@ public class KeyBindingsView extends HighlightableView<KeyBindingsView.State> {
 		keyControls = (HashMap<String, String>) RunGame.KEY_CONTROLS.clone();
 		changeState(State.NORMAL);
 	}
+	
+	/**
+	 * Renders the key bindings screen.
+	 */
 
 	public void render(GL gl, Point point) {
 		GLUtil glutil = new GLUtil(gl);
@@ -149,7 +153,7 @@ public class KeyBindingsView extends HighlightableView<KeyBindingsView.State> {
 			String value = keyControls.get(key);
 			if(value != null) {
 				Text text = strText.get(key);
-				text.setText(value.replaceAll("pad", ""));
+				text.setText(value.replaceAll("Pad", ""));
 
 				if(!key.matches("Move .+"))
 					text.setXYZ(iter.current().getX() + 7 - text.getWidth() / 2, iter.current().getY() - 2, iter.current().getZ());
@@ -161,146 +165,13 @@ public class KeyBindingsView extends HighlightableView<KeyBindingsView.State> {
 			String key = getNameForObject(wheel.current());
 			String value = keyControls.get(key);			
 			Text text = strText.get(key);
-			text.setText(value.replaceAll("pad", ""));
+			text.setText(value.replaceAll("Pad", ""));
 			double angle = Math.PI * 2 * i / 8;
-			text.setXYZ(12 * Math.cos(angle) - text.getWidth() / 2, 12 * Math.sin(angle) - text.getHeight() / 2 + 2.3, -25);
+			text.setXYZ(12.5 * Math.cos(angle) - text.getWidth() / 2, 12.5 * Math.sin(angle) - text.getHeight() / 2 + 2.3, -25);
 		}
 		
 
 		glutil.color(MainMenuView.PLAIN);
 		renderObjects(gl);
 	}
-		
-	
-
-	/**
-	 * Renders the key bindings screen.
-	 */
-/*	public void render(GL gl, Point point) {
-//		GLUtil glutil = new GLUtil(gl);
-//		glutil.standardFrustum(gl, point);
-//		glutil.clearColor(BACKGROUND_COLOR);
-//		gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_SRC_COLOR);
-//		
-//		gl.glTranslated(0, 0, zoom);
-//		
-/*		switch(getState()) {
-		case NORMAL:
-			
-			gl.glLoadIdentity();
-			gl.glTranslated(0, -9.5, -14.5);
-			
-			Highlightable N_ARROWArrow = Button.N_ARROW.getButton();
-			N_ARROWArrow.setXY(0, 14);
-			putHighlightable(N_ARROWArrow);
-			
-			Text N_ARROWCommand = new Text(keyControls.get("N_ARROW"), smallRenderer, 0.075f);
-			N_ARROWCommand.setXY(-0.25 - 0.5*(keyControls.get("N_ARROW").length()/2), 15.5);
-			N_ARROWCommand.render(gl);
-			
-			Highlightable southArrow = Button.S_ARROW.getButton();
-			southArrow.setXY(0, 8);
-			putHighlightable(southArrow);
-			
-			Text southCommand = new Text(keyControls.get("S_ARROW"), smallRenderer, 0.075f);
-			southCommand.setXY(-0.25 - 0.5*(keyControls.get("S_ARROW").length()/2), 5.5);
-			southCommand.render(gl);
-			
-			Highlightable eastArrow = Button.E_ARROW.getButton();
-			eastArrow.setXY(3, 10);
-			putHighlightable(eastArrow);
-			
-			Text eastCommand = new Text(keyControls.get("E_ARROW"), smallRenderer, 0.075f);
-			eastCommand.setXY(4.5, 10.5);
-			eastCommand.render(gl);
-			
-			Highlightable westArrow = Button.W_ARROW.getButton();
-			westArrow.setXY(-3, 10);
-			putHighlightable(westArrow);
-			
-			Text westCommand = new Text(keyControls.get("W_ARROW"), smallRenderer, 0.075f);
-			westCommand.setXY(-6 - 0.75 *(keyControls.get("W_ARROW").length()/2), 10.5);
-			westCommand.render(gl);
-			
-			Highlightable N_ARROWeastArrow = Button.NE_ARROW.getButton();
-			N_ARROWeastArrow.setXY(1, 13);
-			putHighlightable(N_ARROWeastArrow);
-			
-			Text northEastCommand = new Text(keyControls.get("NE_ARROW"), smallRenderer, 0.075f);
-			northEastCommand.setXY(3.5 - 0.5*(keyControls.get("NE_ARROW").length()/2), 14.25);
-			northEastCommand.render(gl);
-			
-			Highlightable southeastArrow = Button.SE_ARROW.getButton();
-			southeastArrow.setXY(1, 9);
-			putHighlightable(southeastArrow);
-			
-			Text southEastCommand = new Text(keyControls.get("SE_ARROW"), smallRenderer, 0.075f);
-			southEastCommand.setXY(3.5 - 0.5*(keyControls.get("SE_ARROW").length()/2), 6.75);
-			southEastCommand.render(gl);
-			
-			Highlightable northWestArrow = Button.NW_ARROW.getButton();
-			northWestArrow.setXY(-1, 13);
-			putHighlightable(northWestArrow);
-			
-			Text northWestCommand = new Text(keyControls.get("NW_ARROW"), smallRenderer, 0.075f);
-			northWestCommand.setXY(-4 - 0.5*(keyControls.get("NW_ARROW").length()/2), 14.25);
-			northWestCommand.render(gl);
-			
-			Highlightable southwestArrow = Button.SW_ARROW.getButton();
-			southwestArrow.setXY(-1, 9);
-			putHighlightable(southwestArrow);
-			
-			Text southWestCommand = new Text(keyControls.get("SW_ARROW"), smallRenderer, 0.075f);
-			southWestCommand.setXY(-4 - 0.5*(keyControls.get("SW_ARROW").length()/2), 7);
-			southWestCommand.render(gl);
-			
-			Text saveGameCommand = new Text(keyControls.get("SAVE_GAME"), smallRenderer, 0.075f);
-			saveGameCommand.setXY(-14.5 - 0.5*(keyControls.get("SAVE_GAME").length()/2), 16.5);
-			saveGameCommand.render(gl);
-			
-			Text loadGameCommand = new Text(keyControls.get("LOAD_GAME"), smallRenderer, 0.075f);
-			loadGameCommand.setXY(-14.5 - 0.5*(keyControls.get("LOAD_GAME").length()/2), 8.5);
-			loadGameCommand.render(gl);
-			
-			Text newGameCommand = new Text(keyControls.get("NEW_GAME"), smallRenderer, 0.075f);
-			newGameCommand.setXY(-15 - 0.5*(keyControls.get("NEW_GAME").length()/2), 0.5);
-			newGameCommand.render(gl);
-			
-			Text inventoryViewCommand = new Text(keyControls.get("INVENTORY_VIEW"), smallRenderer, 0.075f);
-			inventoryViewCommand.setXY(12 - 0.5*(keyControls.get("INVENTORY_VIEW").length()/2), 16.5);
-			inventoryViewCommand.render(gl);
-			
-			Text statsViewCommand = new Text(keyControls.get("STATS_VIEW"), smallRenderer, 0.075f);
-			statsViewCommand.setXY(14 - 0.5*(keyControls.get("STATS_VIEW").length()/2), 8.5);
-			statsViewCommand.render(gl);
-			
-			Text skillsViewCommand = new Text(keyControls.get("SKILLS_VIEW"), smallRenderer, 0.075f);
-			skillsViewCommand.setXY(13.5 - 0.5*(keyControls.get("SKILLS_VIEW").length()/2), 0.5);
-			skillsViewCommand.render(gl);
-			
-			Text optionsViewCommand = new Text(keyControls.get("OPTIONS_VIEW"), smallRenderer, 0.075f);
-			optionsViewCommand.setXY(-0.5 - 0.5*(keyControls.get("OPTIONS_VIEW").length()/2), 0.5);
-			optionsViewCommand.render(gl);
-			
-			Text errorMessage = new Text(message, smallRenderer, 0.075f);
-			errorMessage.setXY(-5, 19);
-			errorMessage.render(gl);
-			
-
-			renderHighlightables(gl);
-			gl.glLoadName(-1);
-			break;
-		case ZOOMING:
-			break;
-		case ZOOMED:
-			break;
-		}
-		
-		gl.glFlush();
-	}
-	
-
-	
-	
-*/
 }
