65d3053cae26634c67a419e6b7ec3371091531e9
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-22 17:26:10 +0000

    updated areaeffect

--- branches/miorel_src/src/rpgeeze/model/ae/AreaEffect.java
+++ branches/miorel_src/src/rpgeeze/model/ae/AreaEffect.java
@@ -1,13 +1,105 @@
 package rpgeeze.model.ae;
 
+import java.util.Hashtable;
+
 import rpgeeze.model.Entity;
 import rpgeeze.model.Visitable;
 import rpgeeze.model.Visitor;
 
-public abstract class AreaEffect implements Visitable {
-	public abstract void apply(Entity entity);
+public abstract class AreaEffect implements Visitable, Cloneable {
 	
+	public static final int UPDATE_RATE = 80;
+	protected final static Hashtable<String, AreaEffect> prototypes = new Hashtable<String, AreaEffect>();
+    protected final String name;
+    protected int rate;
+    protected int counter = UPDATE_RATE;
+    protected boolean messageSent;
+    
+    static {
+        for(AreaEffect ae: new AreaEffect[] {
+                new LevelUp(),
+                new TakeDamage(),
+                new HealDamage(),
+                new InstantDeath(),
+        })
+                prototypes.put(ae.toString(), ae);
+}
+    
+    public AreaEffect(String name) {
+        this(10, name);
+    }
+    
+    public AreaEffect(int rate, String name) {
+        this.name = name;
+        this.rate = rate;
+        messageSent = false;
+    }
+    
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+    
+    public int getRate(){
+        return rate;
+    }
+    public abstract void apply(Entity entity);
+    
+    public abstract void applyEffect(Entity e);
+    
+    public void setMessageSentFlag(boolean flag){
+        messageSent = flag;
+    }
+
+    public static AreaEffect getAreaEffect(String key){
+        return (AreaEffect)prototypes.get(key).clone();
+    }
+    
+    public AreaEffect clone() {
+        AreaEffect ret = null;
+        try {
+                ret = (AreaEffect) super.clone();
+        }
+        catch(CloneNotSupportedException e) {
+        }
+        return ret;
+    }
+    
 	public void accept(Visitor visitor) {
 		visitor.visitAreaEffect(this);
 	}
+	
+	public String toString() {
+        return name;
+	}
 }
+
+
+
+
+
+
+
+
+        
+       
+        
+       
+        
+       
+        
+
+        
+
+        
+        
+        
+       
+        
+
+        
+       
+        
+        
+
+
+
