d30c9430d6013d5077537dcd1449261776ecfa66
mlester <mlester@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-22 00:37:56 +0000

    Visitor to visit all model elements

---
+++ trunk/src/model/xml/GameVisitor.java
@@ -0,0 +1,19 @@
+package model.xml;
+
+import model.*;
+import model.ae.AreaEffect;
+import model.decal.Decal;
+import model.entity.Entity;
+import model.item.Item;
+
+public interface GameVisitor {
+	void visit(Model model);
+	void visit(Map map);
+	void visit(Tile tile);
+	void visit(Terrain terrain);
+	void visit(Location location);
+	void visit(AreaEffect area_effect);
+	void visit(Item item);
+	void visit(Decal decal);
+	void visit(Entity entity);
+}
