89c5ec74349d9a55da13ac82947764e3e60a93c9
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-22 07:58:46 +0000

    added action for back button

--- src/rpgeeze/controller/HelpViewController.java
+++ src/rpgeeze/controller/HelpViewController.java
@@ -1,11 +1,15 @@
 package rpgeeze.controller;
 
+import java.awt.Point;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import javax.media.opengl.glu.GLU;
 
 import rpgeeze.GameManager;
 import rpgeeze.view.HelpView;
 
-public class HelpViewController extends Controller<HelpView> {
+public class HelpViewController extends HighlightableViewController<HelpView> {
 	public HelpViewController(GameManager manager, HelpView view) {
 		super(manager, view);
 	}
@@ -13,5 +17,17 @@ public class HelpViewController extends Controller<HelpView> {
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyChar() == KeyEvent.VK_ESCAPE)
 			getManager().popState();
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		Point p = e.getPoint();
+		if(e.getButton() == MouseEvent.BUTTON1) {
+			String name = getView().pickClosest(GLU.getCurrentGL(), p);
+			if(name != null) {
+				if(name.equals("Back")) {
+					getManager().popState();
+				}
+			}
+		}
 	}
 }
