9a4e8fc35172a2f6998222a2ba616938cbf80483
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-22 14:45:46 +0000

    updated Obstacle

--- branches/miorel_src/src/rpgeeze/model/item/Obstacle.java
+++ branches/miorel_src/src/rpgeeze/model/item/Obstacle.java
@@ -3,10 +3,19 @@ package rpgeeze.model.item;
 import rpgeeze.model.Entity;
 import rpgeeze.model.InaccessibleAreaException;
 
+/**
+ * Makes the Tile that contains it permanently impassable.
+ */
+
+
 public abstract class Obstacle extends Item {
-	public void activate(Entity entity) {
-		// did you just try to walk through an obstacle?
-		// no can do
-		throw new InaccessibleAreaException();
-	}
+    public Obstacle(String name) {
+            super(name);
+    }
+
+    public final void activate(Entity e) throws InaccessibleAreaException {
+            
+    }
+    
 }
+
