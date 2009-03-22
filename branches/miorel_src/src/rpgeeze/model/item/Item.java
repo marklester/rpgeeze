974ca24a24ff2098e19aaf2c86ec5d65a61d622a
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-22 14:32:38 +0000

    added things to Item from trunk branch

--- branches/miorel_src/src/rpgeeze/model/item/Item.java
+++ branches/miorel_src/src/rpgeeze/model/item/Item.java
@@ -1,13 +1,60 @@
 package rpgeeze.model.item;
 
+import java.util.Hashtable;
+import java.util.regex.Pattern;
+
 import rpgeeze.model.Entity;
 import rpgeeze.model.Visitable;
 import rpgeeze.model.Visitor;
 
-public abstract class Item implements Visitable {
-	public abstract void activate(Entity entity);
+public abstract class Item implements Visitable, Cloneable {
+	
+	
+	protected final String name;
+	
+	protected Hashtable<String, String> properties;
+    
+    private static Pattern pattern = Pattern.compile("<item><type>(.*)</type>(.*)</item>");
+    private static Hashtable<String, Item> prototypes = new Hashtable<String, Item>();
+
+
+	public Item(String name){
+		this.name = name;
+	}
 	
 	public void accept(Visitor visitor) {
 		visitor.visitItem(this);
+	}
+	
+	public abstract void activate(Entity entity);
+	
+	public Item clone() {
+         Item ret = null;
+         try {
+                 ret = (Item) super.clone();
+         }
+         catch(CloneNotSupportedException e) {}
+         return ret;
+	}
+	 
+	public void setProperties(Hashtable<String,String> properties){
+         this.properties=properties;
+	}
+	 
+	public Hashtable<String,String> getProperties(){
+         return properties;
+	}
+
+	public boolean isPassable() {
+		return true;
+	}
+	
+	public static Item getItem(String key){
+        return (Item)prototypes.get(key).clone();
+	}
+
+
+	public String toString() {
+		return this.name;
 	}
 }
