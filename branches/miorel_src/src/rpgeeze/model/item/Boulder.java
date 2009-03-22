468f85bfa3b8ffc9b2dff8197dabd5da0367f4c9
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-22 14:50:48 +0000

    added boulder class

---
+++ branches/miorel_src/src/rpgeeze/model/item/Boulder.java
@@ -0,0 +1,13 @@
+package rpgeeze.model.item;
+
+/** 
+ * Obstacle Item
+ */
+
+public class Boulder extends Obstacle {
+
+        public Boulder() {
+                super("Boulder");
+        }
+}
+
