bcc62602921c2db3e1d41e4f8929ddcccf69ff19
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 08:48:20 +0000

    added action to potion life

--- branches/miorel_src/src/rpgeeze/model/item/PotionLife.java
+++ branches/miorel_src/src/rpgeeze/model/item/PotionLife.java
@@ -10,7 +10,7 @@ import rpgeeze.log.Message;
 import rpgeeze.model.entity.Entity;
 
 
-public class PotionLife extends TakeableItem implements EquippableItem{
+public class PotionLife extends TakeableItem {
 
 	public PotionLife() {
 		super("Potion Life");
@@ -19,20 +19,9 @@ public class PotionLife extends TakeableItem implements EquippableItem{
 	
 	
 	public void use(Entity entity){
-		//entity.addHealth(200);
+		entity.addHealth(200);
 		entity.getInventory().removeItem(this);
-		LogManager.getInstance().log("Used Heath Potion.", "", Message.Type.GAME);
+		LogManager.getInstance().log("Used Health Potion.", "", Message.Type.GAME);
 	}
 
-	public PotionLife clone(){
-		return (PotionLife) super.clone();
-	}
-	
-	public void equip(Entity entity){
-		
-	}
-	
-	public void unequip(Entity entity){
-		
-	}
 }
