5f2cdc28326c54fb206936617a7b796bf8a99869
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 04:48:25 +0000

    One shot item

--- branches/miorel_src/src/rpgeeze/model/item/OneShotItem.java
+++ branches/miorel_src/src/rpgeeze/model/item/OneShotItem.java
@@ -1,5 +1,8 @@
 package rpgeeze.model.item;
 
+import rpgeeze.model.Tile;
+import rpgeeze.model.entity.Entity;
+
 /**
  * An Item that is activated and removed from the Map when touched by an
  * Entity.
@@ -9,6 +12,13 @@ public abstract class OneShotItem extends Item {
 	public OneShotItem(String name) {
 		super(name);
 	}
+
+	public final void activate(Entity entity, Tile tile) {
+		doActivate(entity, tile);
+		// tile.setItem(null);
+	}
+	
+	protected abstract void doActivate(Entity entity, Tile tile);
 }
 
 
