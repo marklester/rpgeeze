ee814950ed36c969301ffbc68921c69d2aeeaf83
speedymorales5 <speedymorales5@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 11:34:42 +0000

    Provided Entity Classes for Huge Merge

---
+++ entity3/Soldier.java
@@ -0,0 +1,28 @@
+package model.entity2;
+
+import model.ai.*;
+import view.*;
+
+public class Soldier extends Monster {
+	
+	public Soldier()
+	{
+		super();
+		this.setAI(new AimlessAI());
+	}
+	
+	@Override
+	public void draw(Drawer d) {
+		d.drawEntity(this);
+	}
+	
+	public String toString()
+	{
+		return "Soldier";
+	}
+	
+	public void update()
+	{
+		super.update();
+	}
+}
