f057bc536b1926e56ccc6efba774c0273f186890
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 04:41:27 +0000

    Equipment

--- src/rpgeeze/model/entity/Equipment.java
+++ src/rpgeeze/model/entity/Equipment.java
@@ -2,24 +2,65 @@ package rpgeeze.model.entity;
 
 import rpgeeze.model.item.Item;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
 public class Equipment implements Cloneable {
-	private Item head = null;
-	private Item armor = null;
-	private Item boots = null;
-	private Item weapon = null;
-	private Item auxiliary = null;
+	private Item head;
+	private Item armor;
+	private Item boots;
+	private Item weapon;
+	private Item auxiliary;
 	
-	public Equipment() 	{}
-	public Equipment(Item head,Item armor,Item boots,Item weapon,Item aux){
+	public Equipment() 	{
+		this(null, null, null, null, null);
+	}
+
+	public Equipment(Item head, Item armor, Item boots, Item weapon, Item auxiliary) {
 		this.head = head;
-		this.armor=armor;
-		this.boots=boots;
+		this.armor = armor;
+		this.boots = boots;
 		this.weapon = weapon;
-		this.auxiliary = aux;
-	}	
+		this.auxiliary = auxiliary;
+	}
+	
+	public Item getHead() {
+		return head;
+	}
+
+	public void setHead(Item head) {
+		this.head = head;
+	}
+
+	public Item getArmor() {
+		return armor;
+	}
+
+	public void setArmor(Item armor) {
+		this.armor = armor;
+	}
+
+	public Item getBoots() {
+		return boots;
+	}
+
+	public void setBoots(Item boots) {
+		this.boots = boots;
+	}
+
+	public Item getWeapon() {
+		return weapon;
+	}
+
+	public void setWeapon(Item weapon) {
+		this.weapon = weapon;
+	}
+
+	public Item getAuxiliary() {
+		return auxiliary;
+	}
+
+	public void setAuxiliary(Item auxiliary) {
+		this.auxiliary = auxiliary;
+	}
+
 	public synchronized Equipment clone() {
 		Equipment ret = new Equipment();
 		ret.head = head == null ? null : head.clone();
