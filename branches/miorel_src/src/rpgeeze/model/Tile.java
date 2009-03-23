2203cdced2d347348ddac9deef1c03aa52a692ba
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 01:17:19 +0000

    Game

--- branches/miorel_src/src/rpgeeze/model/Tile.java
+++ branches/miorel_src/src/rpgeeze/model/Tile.java
@@ -47,11 +47,11 @@ public class Tile implements Cloneable, Visitable {
 	}
 	
 	public int getX() {
-		return 0;
+		return location.getX();
 	}
 
 	public int getY() {
-		return 0;
+		return location.getY();
 	}
 	
 	public Terrain getTerrain() {
