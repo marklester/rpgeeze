cc66c0eb0c00f82ab1f95addaf3b3a92c712cdad
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 06:02:39 +0000

    Occupations

--- branches/miorel_src/src/rpgeeze/model/entity/Sneak.java
+++ branches/miorel_src/src/rpgeeze/model/entity/Sneak.java
@@ -1,26 +1,9 @@
 package rpgeeze.model.entity;
 
-import java.util.LinkedList;
-
-import rpgeeze.model.entity.PrimaryStats;
-import rpgeeze.model.entity.Stats;
-import rpgeeze.model.skill.Skill;
-
 public class Sneak extends Occupation {
-	
 	public Sneak() {
-		super("Sneak", new Stats(1,100,20,15,new PrimaryStats(3,5,20,5,2,1)));
-		populateSpecificSkills();
+		super("Sneak", new Stats(1,100,20,15,new PrimaryStats(3,5,20,5,2,1))
+				//new OneHandedWeapon(), new TwoHandedWeapon(), new Brawling()
+		);
 	}
-
-	public Sneak(Stats stats) {
-		super("Sneak", stats);
-		populateSpecificSkills();		
-	}
-	
-	protected Sneak(Stats stats, LinkedList<Skill> skills) {
-		super("Sneak", stats, skills);
-	}
-	
-	public void populateSpecificSkills() { }
 }
