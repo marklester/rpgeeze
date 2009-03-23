76fae546eac41fe3a441b2e3c2b47286d5c68f9c
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 08:50:29 +0000

    added action to mana potion

--- branches/miorel_src/src/rpgeeze/model/item/ManaPotion.java
+++ branches/miorel_src/src/rpgeeze/model/item/ManaPotion.java
@@ -2,15 +2,7 @@ package rpgeeze.model.item;
 
 import rpgeeze.log.LogManager;
 import rpgeeze.log.Message;
-import rpgeeze.model.Tile;
 import rpgeeze.model.entity.Entity;
-import rpgeeze.model.entity.PC;
-
-/** 
- * I haven't really looked into how this is being used, whoever added this
- * should document it (i.e. I am too lazy to do it.)
- */
-
 
 public class ManaPotion extends TakeableItem {
 	public ManaPotion() {
@@ -18,7 +10,7 @@ public class ManaPotion extends TakeableItem {
 	}
 
 	public void use(Entity entity) {
-//		entity.addMana(200);
+		entity.addMana(200);
 		entity.removeItem(this);
 		LogManager.getInstance().log("Used Mana Potion.", "MODEL", Message.Type.GAME);
 	}
