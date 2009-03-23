47dc61220f56e0ed863a7ef14a74b029f6ddf360
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 19:40:08 +0000

    More skill view

--- branches/miorel_src/src/rpgeeze/model/entity/Smasher.java
+++ branches/miorel_src/src/rpgeeze/model/entity/Smasher.java
@@ -2,13 +2,17 @@ package rpgeeze.model.entity;
 
 import rpgeeze.model.entity.PrimaryStats;
 import rpgeeze.model.entity.Stats;
+import rpgeeze.model.skill.Bargain;
+import rpgeeze.model.skill.BindWounds;
 import rpgeeze.model.skill.Brawling;
+import rpgeeze.model.skill.Observation;
 import rpgeeze.model.skill.OneHandedWeapon;
 import rpgeeze.model.skill.TwoHandedWeapon;
 
 public class Smasher extends Occupation {
 	public Smasher() {
 		super("Smasher", new Stats(1,100,20,15,(new PrimaryStats(3,20,5,5,2,1))),
+				new BindWounds(), new Bargain(), new Observation(),
 				new OneHandedWeapon(), new TwoHandedWeapon(), new Brawling()
 		);
 	}
--- branches/miorel_src/src/rpgeeze/model/entity/Summoner.java
+++ branches/miorel_src/src/rpgeeze/model/entity/Summoner.java
@@ -1,9 +1,12 @@
 package rpgeeze.model.entity;
 
+import rpgeeze.model.skill.*;
+
 public class Summoner extends Occupation {
 	public Summoner() {
-		super("Summoner", new Stats(1,100,20,15,new PrimaryStats(3,5,5,20,2,1))
-				//new OneHandedWeapon(), new TwoHandedWeapon(), new Brawling()
+		super("Summoner", new Stats(1,100,20,15,new PrimaryStats(3,5,5,20,2,1)),
+		new BindWounds(), new Bargain(), new Observation(),
+				new OneHandedWeapon(), new TwoHandedWeapon(), new Brawling()
 		);
 	}
 }
