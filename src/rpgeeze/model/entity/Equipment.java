ac35f72d6d516a65ff30acbeb5594fed1c5e4c03
mlester <mlester@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 04:22:57 +0000

    add a constructor for xmlreading

--- src/rpgeeze/model/entity/Equipment.java
+++ src/rpgeeze/model/entity/Equipment.java
@@ -6,17 +6,20 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Equipment implements Cloneable {
-	private static final Pattern pattern = Pattern.compile("<equipment><head>(.*)</head><armor>(.*)</armor><boots>(.*)</boots><weapon>(.*)</weapon><auxiliary>(.*)</auxiliary></equipment>");
-	
 	private Item head = null;
 	private Item armor = null;
 	private Item boots = null;
 	private Item weapon = null;
 	private Item auxiliary = null;
 	
-	public Equipment() 	{
-	}
-	
+	public Equipment() 	{}
+	public Equipment(Item head,Item armor,Item boots,Item weapon,Item aux){
+		this.head = head;
+		this.armor=armor;
+		this.boots=boots;
+		this.weapon = weapon;
+		this.auxiliary = aux;
+	}	
 	public synchronized Equipment clone() {
 		Equipment ret = new Equipment();
 		ret.head = head == null ? null : head.clone();
