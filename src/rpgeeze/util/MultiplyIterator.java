4693d23da2693eba12603b0d13c1349bc16c46f2
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 00:24:10 +0000

    added MultiplyIterator

---
+++ src/rpgeeze/util/MultiplyIterator.java
@@ -0,0 +1,36 @@
+package rpgeeze.util;
+
+import rpgeeze.dp.Iterator;
+
+public class MultiplyIterator<T> implements Iterator<T> {
+    private final Iterator<? extends T> iter;
+    private final int factor;
+    private int counter = 0;
+   
+    public MultiplyIterator(Iterator<? extends T> iterator, int factor) {
+            this.iter = iterator;
+            this.factor = factor;
+    }
+   
+    public void reset() {
+            iter.reset();
+            counter = 0;
+    }
+   
+    public boolean isDone() {
+            return iter.isDone();
+    }
+   
+    public void advance() {
+            ++counter;
+            if(counter == factor) {
+                    counter = 0;
+                    iter.advance();
+            }
+    }
+   
+    public T current() {
+            return iter.current();
+    }
+}
+
