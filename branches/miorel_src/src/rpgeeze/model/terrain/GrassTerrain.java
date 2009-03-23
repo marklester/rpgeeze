c5dc03bc4368570e91bbc5d079d87f8b87383534
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 05:08:48 +0000

    Terrains

--- branches/miorel_src/src/rpgeeze/model/terrain/GrassTerrain.java
+++ branches/miorel_src/src/rpgeeze/model/terrain/GrassTerrain.java
@@ -4,15 +4,12 @@ public class GrassTerrain extends Terrain {
 	private static Terrain instance;
 	
 	private GrassTerrain() {
+		super("Grass Terrain");
 	}
 	
 	public static Terrain getInstance() {
 		if(instance == null)
 			instance = new GrassTerrain();
 		return instance;
-	}
-
-	public String getName() {
-		return "Grass Terrain";
 	}
 }
