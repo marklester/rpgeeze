07598e1112ef2ef210153c41deb2c94e4d6c9d5e
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 07:11:05 +0000

    merged Portal item

--- src/rpgeeze/model/item/Portal.java
+++ src/rpgeeze/model/item/Portal.java
@@ -1,13 +1,11 @@
 package rpgeeze.model.item;
 
-import java.util.HashMap;
-
 import rpgeeze.model.Location;
 import rpgeeze.model.Tile;
 import rpgeeze.model.entity.Entity;
-import rpgeeze.model.entity.PC;
 
-//import java.util.Hashtable;
+
+
 
 public class Portal extends InteractiveItem {
 	private Location where;
@@ -21,41 +19,15 @@ public class Portal extends InteractiveItem {
 		this.where = loc;
 	}
 	
-	public void activate(PC pc) {
-		use(pc);
-	}
 	
-
-	public void use(PC pc) {
-		pc.getTile().releaseEntity();		
-		//AudioThread.getInstance().run(this.name, AudioThread.CLIP);
-		if(where != null) {
-			pc.move(where);
-			Console.getInstance().writeLine("A strange force sweeps you off your feet.");
+	public void activate(Entity entity, Tile tile){
+		if(entity.getEntityType().equals("Playable Character")){
+			entity.getTile().releaseEntity();
+			//AudioThread.getInstance().run(this.name, AudioThread.CLIP);
+			if(where != null){
+				entity.move(where);
+				//put log message here A strange force sweeps you off your feet.
+			}
 		}
-	}
-	
-	protected void setAttributesFromXml(String xml) {
-		if(xml.length() != 0)
-			this.where = Location.fromXml(xml);
-	}
-	
-	public void setProperties(HashMap<String, String> properties) {
-		//Use Properties in Hash Map here
-		super.setProperties(properties);//makes sure save can save properties
-		System.out.println("Portal"+properties.get("location"));
-		String[] loc = properties.get("location").split(",");
-		int x = Integer.parseInt(loc[0]);
-		int y = Integer.parseInt(loc[1]);
-		where = new Location(x,y);
-	}
-
-	public void activate(Entity entity, Tile tile) {
-		
-	}
-
-	public void use(PC pc) {
-		// TODO Auto-generated method stub
-		
 	}
 }
