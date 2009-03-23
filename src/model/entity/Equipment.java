0620e992962e441c5bc930e4c10b7f53e8751dd8
mlester <mlester@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 00:58:56 +0000

    added constuctor for equipment to make making one

--- src/model/entity/Equipment.java
+++ src/model/entity/Equipment.java
@@ -16,7 +16,13 @@ public class Equipment implements Cloneable {
 	
 	public Equipment() 	{
 	}
-		
+	public Equipment(Item head,Item armor,Item boots,Item weapon,Item aux){
+		this.head = head;
+		this.armor=armor;
+		this.boots=boots;
+		this.weapon = weapon;
+		this.auxiliary = aux;
+	}	
 	public String toXml() {
 		return toXml("");
 	}
