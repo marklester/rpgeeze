d77091cc4c6d75fe0f6d941ab95cb6184d4fb649
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 04:46:25 +0000

    Decals

--- src/rpgeeze/model/decal/SkullAndCrossbones.java
+++ src/rpgeeze/model/decal/SkullAndCrossbones.java
@@ -1,15 +1,7 @@
 package rpgeeze.model.decal;
 
 public class SkullAndCrossbones extends Decal {
-	private static SkullAndCrossbones instance = null;
-
 	public SkullAndCrossbones() {
 		super("Skull and Crossbones");
-	}
-
-	public static SkullAndCrossbones getInstance() {
-		if(instance == null)
-			instance = new SkullAndCrossbones();
-		return instance;
 	}
 }
