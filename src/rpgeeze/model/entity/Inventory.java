9448814a3d4af40cb497d540bb89c1ddf463671d
mlpalii <mlpalii@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 04:35:19 +0000

    Inventory done.

--- src/rpgeeze/model/entity/Inventory.java
+++ src/rpgeeze/model/entity/Inventory.java
@@ -1,6 +1,6 @@
 package rpgeeze.model.entity;
 
-/*
+/**
  * An inventory is a set of items carried by an entity. 
  * Items may be added to or dropped from the inventory 
  * and items in the inventory may be equipped.
@@ -16,21 +16,27 @@ import rpgeeze.util.ArrayIterator;
 import rpgeeze.dp.Iterator;
 
 public class Inventory {
-	public static final int INV_MAX_SIZE = 100;
+	public static final int INV_MAX_SIZE = 25;
 
+	private final int maxSize;
 	private ArrayList<Item> items;
 
 	public Inventory() {
+		this(INV_MAX_SIZE);
+	}
+	
+	public Inventory(int maxSize) {
 		items = new ArrayList<Item>();
+		this.maxSize = maxSize;
 	}
 
-	public boolean addItem(Item i, boolean announce) {
+	public boolean addItem(Item item, boolean announce) {
 		LogManager lm = LogManager.getInstance();
 		boolean ret;
-		if(items.size() < INV_MAX_SIZE) {
-			items.add(i);
+		if(items.size() < maxSize) {
+			items.add(item);
 			if(announce)
-				lm.log(i.getName() + " has been added to your inventory.", "INVENTORY", Message.Type.GAME);
+				lm.log(item.getName() + " has been added to your inventory.", "INVENTORY", Message.Type.GAME);
 			ret = true;
 		}
 		else {
@@ -41,7 +47,7 @@ public class Inventory {
 	}
 
 	public Item removeItemAt(int i) {
-		return this.items.remove(i);
+		return items.remove(i);
 	}
 
 	public Item getItemAt(int i) {
