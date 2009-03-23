c5324e826ee9448aefe4b487374ca6b825c5df40
BJones7887 <BJones7887@d8270ddc-eb00-11dd-a550-c997509ed985>
2009-03-23 04:30:43 +0000

    merged gamemanager

--- branches/miorel_src/src/rpgeeze/GameManager.java
+++ branches/miorel_src/src/rpgeeze/GameManager.java
@@ -389,7 +389,7 @@ public class GameManager extends DelegatingEventAdapter
 		matrix[5][5].setItem(new HealthPack());
 		matrix[2][6].setItem(new HealthPack());
 		matrix[1][6].setItem(new Mana());
-		matrix[1][0].setItem(new PortalItem());
+		matrix[1][0].setItem(new Portal());
 		matrix[1][8].setItem(new RedArmor());
 		matrix[1][9].setItem(new Shield());
 		matrix[2][9].setItem(new Sword());
